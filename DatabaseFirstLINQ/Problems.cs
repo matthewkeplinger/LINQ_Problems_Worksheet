7df42f46b359d5ee7edc7d80db371a5243363989
Matthew Keplinger <matthew.keplinger85@gmail.com>
2021-09-23 15:46:31 -0400

    Problems through 20 Completed, Ran, and Tested

--- DatabaseFirstLINQ/Problems.cs
+++ DatabaseFirstLINQ/Problems.cs
@@ -31,10 +31,10 @@ namespace DatabaseFirstLINQ
             //ProblemFourteen();
             //ProblemFifteen();
             //ProblemSixteen();
-            ProblemSeventeen();
+            //ProblemSeventeen();
             //ProblemEighteen();
             //ProblemNineteen();
-            //ProblemTwenty();
+            ProblemTwenty();
         }
 
         // <><><><><><><><> R Actions (Read) <><><><><><><><><>
@@ -286,7 +286,9 @@ namespace DatabaseFirstLINQ
         private void ProblemTwenty()
         {
             // Delete the user with the email "oda@gmail.com" from the Users table using LINQ.
-
+            var user = _context.Users.Where(ur => ur.Email == "oda@gmail.com").SingleOrDefault();
+            _context.Users.Remove(user);
+            _context.SaveChanges();
         }
 
         // <><><><><><><><> BONUS PROBLEMS <><><><><><><><><>
